a4e99529acec2988e4ea1447f7de11c2ebc03e6c
nabin <nkbhaie2017@gmail.com>
2022-03-30 16:38:04 -0600

    write a function to use react state, and apply condition for eightballs

--- EightBall.js
+++ EightBall.js
@@ -6,4 +6,27 @@ function choice(arr) {
   return arr[randomIdx];
 }
 
+
+function EightBall(props) {
+    const [msg, setMsg] = useState("Think of a Question.");
+    const [color, setColor] = useState("black");
+  
+    function handleClick() {
+      const { msg, color } = choice(props.answers);
+      setMsg(msg);
+      setColor(color);
+    }
+  
+    return (
+      <div
+        className="EightBall"
+        onClick={handleClick}
+        style={{ backgroundColor: color }}
+      >
+        <b>{msg}</b>
+      </div>
+    );
+  }
+  
+  
 export default EightBall;
